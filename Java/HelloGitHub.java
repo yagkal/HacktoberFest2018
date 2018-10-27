82cad5d410c506c48ec1169612b51fc047ff8ce0
filozofix <44478738+filozofix@users.noreply.github.com>
2018-10-27 11:53:12 +0300

    Update HelloGitHub.java

--- Java/HelloGitHub.java
+++ Java/HelloGitHub.java
@@ -2,7 +2,7 @@ public class HelloGitHub {
 
 	public static void main(String[] args) {
 
-	    System.out.println("Hello, World!");
+	    System.out.println("Hello, GitHub!");
 	    
 	    //Yukarıdaki komutun sözdizimini kullanarak aşağıya "Hello, GitHub!" yazdırın.
 
