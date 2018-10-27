a0a299deb9b57d416454977a6306d87e7466bb9d
filozofix <44478738+filozofix@users.noreply.github.com>
2018-10-27 11:40:04 +0300

    Update.ArdisikSayi.py

--- ArdışıkSayı.py
+++ ArdışıkSayı.py
@@ -2,8 +2,8 @@
 
 a = input("İlk Terim:")
 b = input("Son Terim:")
-c = input("Artış Miktarı:")
+c = input("Ardışık İki Terimin Farkı:")
 
-sonuç = (((int(b) + int(a)) * (int(b) - int(a) + int(c))) / (2 * int(c)))
+sonuç = (((int(a) + int(b)) * (int(b) - int(a) + int(c))) / (2 * int(c)))
 
 print("Sonuç:", int(sonuç))
