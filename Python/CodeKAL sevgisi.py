014e12621efdea30a609e539ff79003f1b1e651a
Gokceduzyol <32934286+Gokceduzyol@users.noreply.github.com>
2019-10-22 18:06:30 +0300

    Create CodeKAL sevgisi.py

---
+++ Python/CodeKAL sevgisi.py
@@ -0,0 +1,9 @@
+
+#Aşağıdaki komutlar CodeKAL'ı çok seviyorum ve CodeKAL'ın bir parçası olmaktan çok mutluyum! çıktılarını vermektedir.
+print("CodeKAL'ı çok seviyorum")
+print("CodeKAL'ın bir parçası olmaktan çok mutluyum!")
+
+#GÖREV: Yukarıdaki komutların benzerin kullanarak İngilizce çıktılarını veriniz.
+
+#YAZ: Aynı görevi java programlama dili ile de yaptınız. Bu iki dil arasında gördüğünüz farklar nedir?
+# başına "#" (yorum işareti) koyarak düşüncelerinizi kodun sonuna ekleyiniz.
