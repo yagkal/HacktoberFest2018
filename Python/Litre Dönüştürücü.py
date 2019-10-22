1ed4e55759eeafb61a2d42a3c64b74ed38759541
Gokceduzyol <32934286+Gokceduzyol@users.noreply.github.com>
2019-10-22 18:13:30 +0300

    Create Litre Dönüştürücü.py

---
+++ Python/Litre Dönüştürücü.py
@@ -0,0 +1,15 @@
+
+#GÖREV: Baş bırakılan yerleri doldurunuz:
+#Girdileri alıyoruz:
+kenar1 = int(input("1.kenarı giriniz:"))
+kenar2 = #Kodu tamamlayınız
+kenar3 = #Kodu tamamlayınız
+
+#Prizmanın hacmini cm^3 cinsinden giriniz:
+hacimCM3 = #Kodu tamamlayınız
+
+#Prizmanın hacmini litre cinsinden heaplıyoruz:
+hacimL = #Kodu tamamlayınız
+
+#Çıktıyı alıyoruz:
+print("Litre cinsinden hacmimiz: ", hacimL)
