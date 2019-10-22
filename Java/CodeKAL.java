383227764e8166797305649c8e7202f76079d5d1
Gokceduzyol <32934286+Gokceduzyol@users.noreply.github.com>
2019-10-22 17:24:46 +0300

    Update CodeKAL.java

--- Java/CodeKAL.java
+++ Java/CodeKAL.java
@@ -5,8 +5,8 @@ public class CodeKal {
 		// Aşağıdaki cümleleri ingilizceye çeviriniz.(Noktalama işaretlerinin Java için 
 		// özel anlamları var. Çevirirken silmeyiniz.)
 
-		System.out.println("I love CodeKAL.");
-		System.out.println("I'm happy that I'm in CodeKAL.");
+		System.out.println("CodeKAL'ı seviyorum!");
+		System.out.println("CodeKAL'ın bir parçası olduğum için çok mutluyum.");
 
 	}
 
