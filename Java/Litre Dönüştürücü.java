90ea299297ebd6cf0cb4fe0645c642712051a2c2
Gokceduzyol <32934286+Gokceduzyol@users.noreply.github.com>
2019-10-22 18:08:39 +0300

    Create Litre Dönüştürücü.java

---
+++ Java/Litre Dönüştürücü.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class LitreDönüştürücüCevap {
+
+    public static void main(String[] args) {
+        //Inputlarımızı alıyoruz.
+         Scanner scn = new Scanner(System.in);
+        //Dikdörtgenler prizmasının köşelerini cm cinsinden alıyoruz.
+        System.out.println("Birinci  kenarı giriniz: ");
+        int kenar1 = scn.nextInt();
+        System.out.println("İkinci kenarı giriniz: ");
+        int kenar2 = scn.nextInt();
+        System.out.println("Üçüncü kenarı giriniz: ");
+        int kenar3 = scn.nextInt();
+        //Hiçbir zaman scanner'ı kapatmayı unutma.
+        scn.close();
+
+        //GÖREV: Aşağıda eksik bırakılan yerleri tamamlayınız.
+        //Prizmanın hacmini cm^3 cinsinden hesapladık.
+        int hacimCM3 = //Kodu tamamlayınız
+        //Prizmanın hacmini litreye çevirdik.
+        double hacimL = //Kodu tamamlayınız
+
+        //Çıktılarımızı alınız.
+       
+
+    }
+
+}
